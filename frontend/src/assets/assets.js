7ece34aabf1b802b513e86967072d6a0a54d63f5
Bhavesh Kumar Singh <kishornand.dib@gmail.com>
2026-03-17 16:29:08 +0530

    Mounted Components on HomePage

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,23 @@
+import p_img1 from "./p_img1.png";
+
+import cart_icon from "./cart_icon.png";
+import hero_img from "./hero_img.png";
+
+export const assets = {
+    cart_icon,
+    hero_img,
+}
+
+export const products = [
+    {
+        _id: "aaaa",
+        name : "Mens Round Neck Cotton T-Shirt",
+        descripton : "This Round Neck T-shirt will cater a rich in-class and genuine appearance in any kind of ambience and will comfort you with saving time.",
+        price : 100,
+        image: [p_img1],
+        category: "Men",
+        subCategory: "Topwear",
+        sizes: ["S", "M", "L", "XL"],
+        bestseller : true,
+    }
+]
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import Hero from "../components/Hero.jsx";
+import LatestCollection from '../components/LatestCollection.jsx';
+import BestSeller from '../components/BestSeller.jsx';
 
-const Home = () => {
+function Home() {
   return (
     <div>
-      
+      <Hero/>
+      <LatestCollection/>
+      <BestSeller/>
     </div>
   )
 }
 
-export default Home
+export default Home;
