2b221895c8f93add2803dae28cd13e10b42e887c
Bhavesh Kumar Singh <kishornand.dib@gmail.com>
2026-03-18 15:53:24 +0530

    Created Newsletter Component

---
+++ frontend/src/components/NewsletterBox.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function NewsletterBox (){
+
+  const onSubmitHandler = (event)=>{
+      event.preventDefault();
+  };
+
+  return (
+    <div className="text-center">
+        <p className="text-2xl font-medium text-gray-800">Subscribe now & get 20% off</p>
+        <p className="text-gray-400 mt-3">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt ut, esse delectus numquam consectetur placeat facilis cupiditate quod. Officia distinctio quas tenetur sequi voluptatibus delectus hic culpa nisi, magnam dolorem?
+        </p>
+        <form onSubmit={onSubmitHandler} className="w-full sm:w-1/2 flex items-center gap-3 mx-auto my-6 border pl-3 ">
+            <input className="w-full sm:flex-1 outline-none" type="email" placeholder="Enter Your Email" required/>
+            <button type="submit" className="bg-black text-white text-xs px-10 py-4">SUBSCRIBE</button>
+        </form>
+    </div>
+  )
+}
+
+export default NewsletterBox
