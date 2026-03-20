eae50421357f870930f768a4a316303341e6d3ff
Bhavesh Kumar Singh <kishornand.dib@gmail.com>
2026-03-20 17:12:30 +0530

    feat: mounted searchbar on the web app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Routes, Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from './pages/Home.jsx';
 import Collection from "./pages/Collection.jsx";
 import About from "./pages/About.jsx";
@@ -11,23 +11,25 @@ import PlaceOrder from "./pages/PlaceOrder.jsx";
 import Orders from "./pages/Orders.jsx"
 import Navbar from './components/Navbar.jsx';
 import Footer from './components/Footer.jsx';
+import SearchBar from './components/SearchBar.jsx';
 
 const App = () => {
   return (
     <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
       <Navbar />
+      <SearchBar />
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/collection" element={<Collection/>}/>
-        <Route path="/about" element={<About/>}/>
-        <Route path="/contact" element={<Contact/>} />
-        <Route path="/product/:productId" element={<Product/>}/>
-        <Route path="/cart" element={<Cart/> }/>
-        <Route path="/login" element={<Login/>} />
-        <Route path="/place-order" element={<PlaceOrder/>} />
-        <Route path="/Orders" element={<Orders/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/collection" element={<Collection />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/product/:productId" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/place-order" element={<PlaceOrder />} />
+        <Route path="/Orders" element={<Orders />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </div>
   )
 }
