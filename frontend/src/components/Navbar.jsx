5e833a1f14c6344402347b8d49e423e52258bd56
Bhavesh Kumar Singh <kishornand.dib@gmail.com>
2026-03-26 17:17:35 +0530

    feat: Added feature to dyanmically chage carticon superscript

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -8,7 +8,7 @@ function Navbar() {
     //visibility for the number icon on the cart displaying the number of products
     const [visible, setVisible] = useState(false);
 
-    const { showSearch, setShowSearch } = useContext(ShopContext);
+    const { showSearch, setShowSearch, getCartCount} = useContext(ShopContext);
 
     return (
         <div className="flex items-center justify-between ">
@@ -51,7 +51,7 @@ function Navbar() {
                 </div>
                 <Link to="/cart" className="relative">
                     <img src={assets.cart_icon} className="w-5 min-w-5" alt="" />
-                    <p className="absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]">10</p>
+                    <p className="absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]">{getCartCount()}</p>
                 </Link>
                 <img onClick={() => setVisible(true)} src={assets.cart_icon} className="w-5 cursor-pointer sm:hidden" alt="" />
             </div>
