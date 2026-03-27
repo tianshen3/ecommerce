5fada5def2d05f0567701e8051e732c4005ab17a
Bhavesh Kumar Singh <kishornand.dib@gmail.com>
2026-03-27 17:01:30 +0530

    feat: Created Link for ProfileIcon

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -8,7 +8,7 @@ function Navbar() {
     //visibility for the number icon on the cart displaying the number of products
     const [visible, setVisible] = useState(false);
 
-    const { showSearch, setShowSearch, getCartCount} = useContext(ShopContext);
+    const { showSearch, setShowSearch, getCartCount, navigate} = useContext(ShopContext);
 
     return (
         <div className="flex items-center justify-between ">
@@ -40,11 +40,11 @@ function Navbar() {
                 <img onClick={() => setShowSearch(!showSearch)} src={assets.cart_icon} className="w-5 cursor-pointer" alt="seacrh_icon" />
 
                 <div className="group relative">
-                    <img className="w-5 cursor-pointer" src={assets.cart_icon} alt="" />
+                    <Link to="/login"><img className="w-5 cursor-pointer" src={assets.cart_icon} alt="profileicon" /></Link>
                     <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4">
                         <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded">
                             <p className="cursor-pointer hover:text-black">My Profile</p>
-                            <p className="cursor-pointer hover:text-black">Orders</p>
+                            <p onClick={()=>navigate('/orders')} className="cursor-pointer hover:text-black">Orders</p>
                             <p className="cursor-pointer hover:text-black">Logout</p>
                         </div>
                     </div>
