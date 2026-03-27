0a6c09235344b8f7b88bae1cda69a1103c23ae9f
Bhavesh Kumar Singh <kishornand.dib@gmail.com>
2026-03-27 17:00:44 +0530

    feat: Bug fixes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -29,7 +29,7 @@ const App = () => {
         <Route path="/cart" element={<Cart />} />
         <Route path="/login" element={<Login />} />
         <Route path="/place-order" element={<PlaceOrder />} />
-        <Route path="/Orders" element={<Orders />} />
+        <Route path="/orders" element={<Orders />} />
       </Routes>
       <Footer />
     </div>
