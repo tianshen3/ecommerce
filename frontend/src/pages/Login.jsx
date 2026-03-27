258ad06df98d231b6fa06739d0a012053434765f
Bhavesh Kumar Singh <kishornand.dib@gmail.com>
2026-03-27 17:03:41 +0530

    feat: OndubmitHandler

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -3,8 +3,13 @@ import React, { useState } from 'react'
 function Login() {
 
   const [currentState, setCurrentState] = useState("Login");
+
+  const onSubmitHandler = async(event) => {
+    event.preventDefault();
+  }
+
   return (
-    <form className="flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800">
+    <form onSubmit={on} className="flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800">
       <div className="inline-flex items-center gap-2 mb-2 mt-10">
         <p className="prata-regular text-3xl">{currentState}</p>
         <hr className="border-none h-[1.5px] w-8 bg-gray-800"/>
