757ceb5cce9ab04886a587ee98116803b8695eab
Bhavesh Kumar Singh <kishornand.dib@gmail.com>
2026-03-29 19:58:19 +0530

    feat: Login Page Bug fixes

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -9,7 +9,7 @@ function Login() {
   }
 
   return (
-    <form onSubmit={on} className="flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800">
+    <form onSubmit={()=>onSubmitHandler()} className="flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800">
       <div className="inline-flex items-center gap-2 mb-2 mt-10">
         <p className="prata-regular text-3xl">{currentState}</p>
         <hr className="border-none h-[1.5px] w-8 bg-gray-800"/>
