704b44c550c9cf8a9afbeba2447baedcfc0c1b3d
Bhavesh Kumar Singh <kishornand.dib@gmail.com>
2026-03-30 19:56:20 +0530

    feat: Mounting on App

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,18 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "./components/Navbar.jsx";
 import Sidebar from "./components/Sidebar.jsx";
+import Login from "./components/Login.jsx";
+ import { ToastContainer } from 'react-toastify';
+import { Route, Routes } from "react-router-dom";
+import Add from "./pages/Add.jsx";
+import List from "./pages/List.jsx";
+import Orders from "./pages/Orders.jsx";
 
+
+export const backendUrl = import.meta.env.VITE_BACKEND_URL
 function App() {
+
+  const[token, setToken] = useState(localStorage.getItem("token") ? localStorage.getItem("token") : "");
+
+  useEffect(()=>{
+    localStorage.setItem("token", token);
+  },[token]);
+
   return(
     <div className="bg-gray-50 min-h-screen">
+      <ToastContainer/>
+    {token==="" ?
+      <Login setToken={setToken}/> :  
       <>
-      <Navbar/>
+      <Navbar setToken={setToken}/>
       <hr></hr>
       <div className="flex w-full">
         <Sidebar/>
+        <div className="w-[70%] mx-auto ml-[max(5vw, 25px] my-8 text-gray-600 text-base">
+          <Routes>
+            <Route path="/add" element={<Add token={token}/>}/>
+            <Route path="/list" element={<List token={token}/>}/>
+            <Route path="/orders" element={<Orders token={token}/>}/>
+          </Routes>
+        </div>
       </div>
       </>
-      
+    }  
     </div>
   )
 }
