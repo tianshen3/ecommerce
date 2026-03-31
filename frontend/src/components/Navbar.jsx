f414fe2afab6019d9d614644502ebb5269543263
Bhavesh Kumar Singh <kishornand.dib@gmail.com>
2026-03-31 22:04:19 +0530

    feat: Implemented Accesstoken based logout functionality

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -8,7 +8,14 @@ function Navbar() {
     //visibility for the number icon on the cart displaying the number of products
     const [visible, setVisible] = useState(false);
 
-    const { showSearch, setShowSearch, getCartCount, navigate} = useContext(ShopContext);
+    const { showSearch, setShowSearch, getCartCount, navigate, token, setToken, setCartItems} = useContext(ShopContext);
+
+    const logout = () =>{
+        navigate("/login");
+        localStorage.removeItem("token");
+        setToken("");
+        setCartItems({});        
+    }
 
     return (
         <div className="flex items-center justify-between ">
@@ -40,14 +47,16 @@ function Navbar() {
                 <img onClick={() => setShowSearch(!showSearch)} src={assets.cart_icon} className="w-5 cursor-pointer" alt="seacrh_icon" />
 
                 <div className="group relative">
-                    <Link to="/login"><img className="w-5 cursor-pointer" src={assets.cart_icon} alt="profileicon" /></Link>
-                    <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4">
+                    <img onClick={()=> token ? null : navigate("/login")}className="w-5 cursor-pointer" src={assets.cart_icon} alt="profileicon" />
+
+                    {/* Drop Down */}
+                    {token && <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4">
                         <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded">
                             <p className="cursor-pointer hover:text-black">My Profile</p>
                             <p onClick={()=>navigate('/orders')} className="cursor-pointer hover:text-black">Orders</p>
-                            <p className="cursor-pointer hover:text-black">Logout</p>
+                            <p onClick={logout} className="cursor-pointer hover:text-black">Logout</p>
                         </div>
-                    </div>
+                    </div>}
                 </div>
                 <Link to="/cart" className="relative">
                     <img src={assets.cart_icon} className="w-5 min-w-5" alt="" />
