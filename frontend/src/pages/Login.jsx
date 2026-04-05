8a51c36edf8f396fcc398de7dca9dedfa5191855
Bhavesh Kumar Singh <kishornand.dib@gmail.com>
2026-04-05 14:07:20 +0530

    feat: Axios error display handling

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -44,7 +44,7 @@ function Login() {
       }
     } catch (error) {
       console.log(error);
-      toast.error(error.message);
+      toast.error(error.response?.data?.message || error.message);
     }
   }
 
